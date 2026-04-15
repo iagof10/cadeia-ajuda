8c565c75918cfe8a01af2e70105f52f871b61d3b
Ivis Rocha <ivis.rocha@veiculoverificado.com.br>
2026-04-15 00:59:43 -0300

    Correção de menu que bugava as vezes

--- CadeiaAjuda.Web/wwwroot/js/blazor-interop.js
+++ CadeiaAjuda.Web/wwwroot/js/blazor-interop.js
@@ -1,6 +1,16 @@
 // Reinitializa o menu do template após navegação Blazor
-// Reinitializa o menu do template após navegação Blazor
 window.initTemplateMenu = function () {
+    // Re-add has-sub class to li elements with ul children
+    $('.navigation').find('li').has('ul').addClass('has-sub');
+
+    // Re-apply active parent classes for compact menu
+    var menuType = $('body').data('menu');
+    if (menuType === 'vertical-compact-menu') {
+        $(".main-menu-content").find('li.active').parents('li:not(.nav-item)').addClass('open');
+        $(".main-menu-content").find('li.active').parents('li').addClass('active');
+    }
+
+    // Re-init menu if available
     if (typeof $.app !== 'undefined' && typeof $.app.menu !== 'undefined') {
         $.app.menu.init();
     }
